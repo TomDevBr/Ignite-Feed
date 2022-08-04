e88e1aafebdb7bb6d361563abc30a0cbf15c6ad0
TomDevBr <tomasjocalheiros@gmail.com>
2022-08-04 20:33:50 -0300

    Estrutura da aplicação: Finzalizando SideBar

--- aula-01/src/components/Sidebar.jsx
+++ aula-01/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react'
 
 export function Sidebar () {
     return (
@@ -9,12 +10,14 @@ export function Sidebar () {
             />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/TomDevBr.png" />
                 <strong>Tomás Calheiros</strong>
                 <span>Web developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20}/>
                     Editar seu Perfil
                 </a>
             </footer>
