2cdafe0d2d263b4ad3ac7175ceaac085bec94882
TomDevBr <tomasjocalheiros@gmail.com>
2022-11-15 10:50:23 -0300

    Estrutura da aplicação - Componente: Avatar

--- aula-01/src/components/Sidebar.jsx
+++ aula-01/src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import styles from './Sidebar.module.css'
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 
 export function Sidebar () {
     return (
@@ -10,7 +11,7 @@ export function Sidebar () {
             />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/TomDevBr.png" />
+                <Avatar src="https://github.com/TomDevBr.png"/>
                 <strong>Tomás Calheiros</strong>
                 <span>Web developer</span>
             </div>
