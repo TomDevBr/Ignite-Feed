97b2820b51a9da87636ad3c29910f2291099c7eb
TomDevBr <tomasjocalheiros@gmail.com>
2022-11-16 19:13:53 -0300

    Os motores do React - Iterando no JSX

--- aula-01/src/App.jsx
+++ aula-01/src/App.jsx
@@ -6,7 +6,36 @@ import { Sidebar } from './components/Sidebar';
 import styles from './App.module.css';
 import './global.css';
 
-
+const posts = [
+  { 
+    id:1,
+    author: {
+      avatarUrl: 'https://github.com/TomDevBr.png',
+      name: 'Tomás Calheiros',
+      role: 'Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-11-10 15:00:00')
+  },
+  {
+    id:2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-11-16 15:00:00')
+  }
+]
 function App() {
   return (
     <> 
@@ -14,7 +43,15 @@ function App() {
     <div className={styles.wrapper}>
       <Sidebar/>
       <main>
-        <Post/>
+        {posts.map(post => {
+          return (
+            <Post
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          )
+        })}
       </main>
     </div>
     </>
--- aula-01/src/components/Post.jsx
+++ aula-01/src/components/Post.jsx
@@ -16,19 +16,8 @@ export function Post() {
                 <time title='11 de Agosto às 09:30h' dataTime="2022-08-11 09:30:50">Publicado há 1h</time>
             </header>
 
-            <div className={styles.content}>
-               <p> Fala galeraa 👋</p>
-
-               <p> Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-               <p><a href=""> jane.design/doctorcare</a></p>
-
-               <p>
-                    <a href=""> #novoprojet</a>{' '}
-                    <a href=""> #nlw</a>{' '} 
-                    <a href=""> #rocketseat</a>
-               </p>
-            </div>
+            
+          
 
             <form className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
