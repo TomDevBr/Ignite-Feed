c2751d0a22e081ab0a55f9bbbe26686f0461884c
TomDevBr <tomasjocalheiros@gmail.com>
2022-11-29 21:29:21 -0300

    Os motores do React - Entendendo a key

--- aula-01/src/components/Post.jsx
+++ aula-01/src/components/Post.jsx
@@ -6,10 +6,6 @@ import { Comment } from './Comment';
 import { Avatar } from './Avatar';
 import { useState } from 'react';
 
-const comments = [
-    1,
-    2,
-]
 
 export function Post({ author, publishedAt, content }) {
     const [comments, setComments] = useState([ 'Post muito bacana, hein!']);
@@ -56,9 +52,9 @@ export function Post({ author, publishedAt, content }) {
             <div className={styles.content}>
                 { content.map(line => {
                     if (line.type = 'paragraph'){
-                        return <p>{line.content}</p>
+                        return <p key={line.content}>{line.content}</p>
                     }else if (line.type === 'link') {
-                        return  <p><a href="#">{    line.content}</a></p>
+                        return  <p key={line.content}><a href="#">{    line.content}</a></p>
                             
                     }
                 })}
@@ -76,7 +72,7 @@ export function Post({ author, publishedAt, content }) {
 
             <div className={styles.commentList}>
                 { comments.map(comment => {
-                    return <Comment content={comment}/>
+                    return <Comment key={comment} content={comment}/>
                 })}
             </div>
         </article>
